b851f67f006c4b3a59b871c81fe1e2a279f89159
meromnt <merislxmnt@gmail.com>
2022-12-05 19:30:59 +0100

    Added a function that plays one round of RPS

--- javascript.js
+++ javascript.js
@@ -3,11 +3,33 @@ function getComputerChoice() {
     let random = rps[Math.floor(Math.random() * rps.length)];
     return random;
   }
-  function playRound (playerSelection, computerSelection){
 
+
+
+
+  function playRound (playerSelection, computerSelection){
+   if (playerSelection == "rock" && computerSelection == "scissors"){
+    let text = "You win, rock beats scissors!";
+    return text;
+   }
+   else if (playerSelection == "paper" && computerSelection == "rock") {
+text = "You win , paper beats rock!";
+return text;
+   }
+   else if (playerSelection == "scissors" && computerSelection == "paper"){
+    text = "You win , scissors beat paper!";
+    return text;
+   }
+   else {
+    text = "You lose";
+    return text;
+   }
   }
 
-  const playerSelection = "rock";
+  
+  let playerSelection = ["rock" , "paper" , "scissors"];
+  playerSelection = prompt("Choose your weapon!")
+  playerSelection = playerSelection.toUpperCase()
+  playerSelection = playerSelection.toLowerCase()
   const computerSelection = getComputerChoice();
-   console.log(playRound(playerSelection, computerSelection));
-
+  console.log(playRound(playerSelection, computerSelection));
