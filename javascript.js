a174082e7225202f00e7ac5a539dca5ab8862a0f
meromnt <merislxmnt@gmail.com>
2022-12-08 18:18:47 +0100

    Added the game function and the finishing touches

--- javascript.js
+++ javascript.js
@@ -10,26 +10,69 @@ function getComputerChoice() {
   function playRound (playerSelection, computerSelection){
    if (playerSelection == "rock" && computerSelection == "scissors"){
     let text = "You win, rock beats scissors!";
+    console.log(playerScore++ , computerScore);
     return text;
+
    }
    else if (playerSelection == "paper" && computerSelection == "rock") {
 text = "You win , paper beats rock!";
+ console.log(playerScore++ , computerScore);
 return text;
+
    }
    else if (playerSelection == "scissors" && computerSelection == "paper"){
+    console.log (playerScore++ , computerScore)
+    
     text = "You win , scissors beat paper!";
     return text;
    }
    else {
-    text = "You lose";
+      console.log(playerScore , computerScore++);
+    text = "You lose, computer wins round!";
     return text;
    }
   }
-
   
   let playerSelection = ["rock" , "paper" , "scissors"];
   playerSelection = prompt("Choose your weapon!")
   playerSelection = playerSelection.toUpperCase()
   playerSelection = playerSelection.toLowerCase()
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
+  let computerSelection = getComputerChoice();
+   let playerScore = 0;
+   let computerScore = 0;
+ 
+
+
+  
+
+  function game () {
+   
+   for (let i = 1;i < 6; i++) {
+      playerSelection = prompt("Choose your weapon!")
+      computerSelection = getComputerChoice()
+      playRound(playerSelection , computerSelection);
+         console.log("round over:"+i);
+         console.log(text);
+         if ( i == 5) {
+          if (playerScore > computerScore) {
+            console.log("Player Wins!")
+          }
+          else {
+            console.log("Computer Wins!")
+          }
+         }
+         
+        
+
+         
+
+
+
+
+   }
+} 
+console.log(game(playRound(playerSelection , computerSelection)));
+  
+
+
+
